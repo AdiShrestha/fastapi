453d5591a4f6219937f599365082e338abb8367f
Aditya Shrestha <yours.only.adi@gmail.com>
2024-05-17 08:15:12 +0545

    Global host added

--- simplefile.py
+++ simplefile.py
@@ -1,7 +1,7 @@
 import requests
 
 def checkWord(x: str):
-    url = "http://localhost:8000/evaluate_text/"
+    url = "https://fastapiaimodel.azurewebsites.net/evaluate_text"
     data = {"text": x}
     response = requests.post(url, json=data)
     if response.json()["detail"] == "Offensive":
@@ -9,12 +9,12 @@ def checkWord(x: str):
     elif response.json()["detail"] == "Not Offensive":
         return False
 
-# while True:
-#     user_input = input("Enter a string (type 'exit' to quit): ")
-#     if user_input == 'exit':
-#         break
-#     if checkWord(user_input):
-#         print("Offensive")
-#     else:
-#         print("Not Offensive")
+while True:
+    user_input = input("Enter a string (type 'exit' to quit): ")
+    if user_input == 'exit':
+        break
+    if checkWord(user_input):
+        print("Offensive")
+    else:
+        print("Not Offensive")
     
